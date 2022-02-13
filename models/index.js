c351618129375ae48d45224b59375c6fd602acaa
Jennifer Cole <capauldi@gmail.com>
2022-02-13 02:30:04 -0800

    associations fixed and working

--- models/index.js
+++ models/index.js
@@ -16,16 +16,12 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  as: 'product_tag',
-  foreignKey: 'product_id'
+  through: ProductTag
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  as: 'product_tag',
-  foreignKey: 'tag_id'
+  through: ProductTag
 });
 
 module.exports = {
